61063f8f9e64d4b257b6011ba8c9a456f2017efc
Steffen kroggel <kroggel@rkw.de>
2019-02-27 12:32:27 +0100

    [UPDATE] Updaing dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,15 +21,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '7.6.10',
+	'version' => '7.6.11',
 	'constraints' => array(
 		'depends' => array(
             'extbase' => '7.6.0-8.7.99',
             'fluid' => '7.6.0-8.7.99',
             'typo3' => '7.6.0-8.7.99',
-			'rkw_basics' => '7.6.10-8.7.99',
-			'rkw_mailer' => '7.6.13-8.7.99',
-            'rkw_registration' => '7.6.10-8.7.99'
+			'rkw_basics' => '8.7.3-8.7.99',
+			'rkw_mailer' => '8.7.1-8.7.99',
+            'rkw_registration' => '8.7.0-8.7.99'
 		),
 		'conflicts' => array(
 		),
